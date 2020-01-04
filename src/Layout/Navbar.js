476b7e6492faaa7d0fa6bd3ae860d6e058114144
Aniketh <anikethnair97@gmail.com>
2020-01-04 07:34:19 +0530

    File structure complete

--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import {
+    Toolbar,
+    AppBar,
+} from '@material-ui/core';
+import { Link, Events } from 'react-scroll';
 
 const Navbar = () => {
+    useEffect(() => {
+        Events.scrollEvent.register('begin', function(to, element) {
+          });
+       
+          Events.scrollEvent.register('end', function(to, element) {
+          });
+       
+    
+          return()=>{
+            Events.scrollEvent.remove('begin');
+            Events.scrollEvent.remove('end');
+          }
+    }, [])
     return (
         <React.Fragment>
-            <div>
-                Navbar
-            </div>
+            <AppBar 
+                position="fixed"
+                style={{backgroundColor:'#ffffff'}}
+                >
+            <Toolbar>
+            </Toolbar>
+            </AppBar>
         </React.Fragment>
     );
 }
