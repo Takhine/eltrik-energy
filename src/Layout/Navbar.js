226fcc5c39d1a2455c6753b73abf50544e87c384
Aniketh <anikethnair97@gmail.com>
2020-01-06 07:30:14 +0530

    Navbar Styled

--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -1,32 +1,70 @@
-import React,{useEffect} from 'react';
+import React, { useEffect,useState } from 'react';
 import {
-    Toolbar,
+    IconButton,
     AppBar,
+    Toolbar,
+    Typography,
+    Button,
+    Drawer
 } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
 import { Link, Events } from 'react-scroll';
 
+const NavMenu = () => {
+    return (
+        <ul className="nav-menu">
+            <li><Button color="inherit">Home</Button></li>
+            <li><Button color="inherit">Dealership</Button></li>
+            <li><Button color="inherit">Benefits</Button></li>
+            <li><Button color="inherit">Eltrik Avenue</Button></li>
+            <li><Button color="inherit">About</Button></li>
+            <li><Button color="inherit" className="download-button">Download</Button></li>
+        </ul>
+    );
+}
 const Navbar = () => {
+    const [drawerOpen,setDrawerOpen]=useState(false);
     useEffect(() => {
-        Events.scrollEvent.register('begin', function(to, element) {
-          });
-       
-          Events.scrollEvent.register('end', function(to, element) {
-          });
-       
-    
-          return()=>{
+        Events.scrollEvent.register('begin', function (to, element) {
+        });
+
+        Events.scrollEvent.register('end', function (to, element) {
+        });
+
+        return () => {
             Events.scrollEvent.remove('begin');
             Events.scrollEvent.remove('end');
-          }
+        }
     }, [])
+    const toggleDrawer = (open) => event => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+          return;
+        }
+    
+        setDrawerOpen(open);
+      };
     return (
         <React.Fragment>
-            <AppBar 
+            <AppBar
                 position="fixed"
-                style={{backgroundColor:'#ffffff'}}
-                >
-            <Toolbar>
-            </Toolbar>
+                id="navbar"
+                style={{backgroundColor:'white'}}
+            >
+                <Toolbar>
+                    <IconButton onClick={toggleDrawer(true)} edge="start" className="menu-button" color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Drawer className="menu-drawer" open={drawerOpen} onClose={toggleDrawer( false)}>
+                        <NavMenu />
+                    </Drawer>
+                    <h2>Eltrik Energy</h2>
+                    <div className="grow" />
+                    <div className="section-desktop">
+                    <NavMenu />
+                    </div>
+                    <Button className="mobile-button">Download</Button>
+                </Toolbar>
             </AppBar>
         </React.Fragment>
     );
