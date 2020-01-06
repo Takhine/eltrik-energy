296f689be81f44f5eb05e6c8466ee89e897d401e
Aniketh <anikethnair97@gmail.com>
2020-01-06 07:58:43 +0530

    Navbar Triggers

--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -1,16 +1,45 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
 import {
     IconButton,
     AppBar,
     Toolbar,
-    Typography,
     Button,
-    Drawer
+    Drawer,
+    useScrollTrigger,
+    Slide
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
 import { Link, Events } from 'react-scroll';
 
+const HideonScroll = (props) => {
+    const { children, window } = props;
+
+    const trigger = useScrollTrigger({ 
+        target: window ? window() : undefined });
+        
+        const trigger2 = useScrollTrigger({ 
+            disableHysteresis: true,
+            threshold: 0
+        });
+
+    return (
+        
+        <Slide appear={false} direction="down" in={!trigger} elevation={trigger2 ? 4 : 0}>
+            {children}
+        </Slide>
+    )
+}
+HideonScroll.propTypes = {
+    children: PropTypes.element.isRequired,
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window: PropTypes.func,
+  };
 const NavMenu = () => {
     return (
         <ul className="nav-menu">
@@ -23,8 +52,8 @@ const NavMenu = () => {
         </ul>
     );
 }
-const Navbar = () => {
-    const [drawerOpen,setDrawerOpen]=useState(false);
+const Navbar = (props) => {
+    const [drawerOpen, setDrawerOpen] = useState(false);
     useEffect(() => {
         Events.scrollEvent.register('begin', function (to, element) {
         });
@@ -39,34 +68,34 @@ const Navbar = () => {
     }, [])
     const toggleDrawer = (open) => event => {
         if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-          return;
+            return;
         }
-    
+
         setDrawerOpen(open);
-      };
+    };
     return (
-        <React.Fragment>
+        <HideonScroll {...props}>
             <AppBar
                 position="fixed"
                 id="navbar"
-                style={{backgroundColor:'white'}}
+                style={{ backgroundColor: 'white' }}
             >
                 <Toolbar>
                     <IconButton onClick={toggleDrawer(true)} edge="start" className="menu-button" color="inherit" aria-label="menu">
                         <MenuIcon />
                     </IconButton>
-                    <Drawer className="menu-drawer" open={drawerOpen} onClose={toggleDrawer( false)}>
+                    <Drawer className="menu-drawer" open={drawerOpen} onClose={toggleDrawer(false)}>
                         <NavMenu />
                     </Drawer>
-                    <h2>Eltrik Energy</h2>
+                    <Button className="logo"><h2>Eltrik Energy</h2></Button>
                     <div className="grow" />
                     <div className="section-desktop">
-                    <NavMenu />
+                        <NavMenu />
                     </div>
                     <Button className="mobile-button">Download</Button>
                 </Toolbar>
             </AppBar>
-        </React.Fragment>
+        </HideonScroll>
     );
 }
 export default Navbar;
